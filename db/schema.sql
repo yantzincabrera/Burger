e59df9ebd29b839a38b775559cf73e070815cc4b
Yantzin Cabrera <yantzincabrera@gmail.com>
2019-09-28 13:57:31 -0700

    schema sql

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE burgers_db;
+USE burgers_db
+CREATE TABLE burgers_db {
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+}
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burgers_db, devoured) VALUES ("Cheese Burger", false);
+INSERT INTO burgers (burgers_db, devoured) VALUES ("Chicken Burger", false);
+INSERT INTO burgers (burgers_db, devoured) VALUES ("Vegan Burger", false);
+INSERT INTO burgers (burgers_db, devoured) VALUES ("Cheese Bacon Burger", false);
